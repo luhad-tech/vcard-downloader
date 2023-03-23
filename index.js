04e500b633cad142a6e9df6e11135976fbc46bdd
Exehad <exehad701@gmail.com>
2023-03-23 12:19:56 -0400

    feat: :sparkles: Got simple link working
    It will download vcf file

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+app.use(express.static('public'))
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.redirect('https://luhad.tech')
+})
+
+app.get('/jlusby', (req, res) => {
+  res.download('./cards/joy_lusby.vcf')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
