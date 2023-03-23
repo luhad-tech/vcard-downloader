b3c1bb7795edeb5db5e693507f7cf0874828bf5a
Hayden <35789234+Exehad@users.noreply.github.com>
2023-03-23 13:21:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.redirect('https://luhad.tech')
-})
-
 app.get('/:org/:person', function (req, res) {
     const org = req.params.org
     const person = req.params.person
     
     res.download(`./cards/${org}/${person}.vcf`)
+})
+
+app.get( (req, res) => {
+  res.redirect('https://luhad.tech')
 })
 
 app.listen(port, () => {
