b1a1e299dea32932fbd6f38cd64a114721c54d0f
Exehad <exehad701@gmail.com>
2023-03-23 13:32:37 -0400

    added simple error handling

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/:org/:person', function (req, res) {
+app.get('/:org/:person', function (req, res, next) {
     const org = req.params.org
     const person = req.params.person
     
-    res.download(`./cards/${org}/${person}.vcf`)
+    res.download(`./cards/${org}/${person}.vcf`, function (err) {
+      if (err) {
+        res.redirect('/')
+      }
+    })
 })
 
-app.get( (req, res) => {
+app.get('*', (req, res) => {
   res.redirect('https://luhad.tech')
 })
 
