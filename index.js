f9a6897e2f973fc091fd9db56d190f78f8392528
Hayden <hayden@luhad.tech>
2023-04-16 16:21:36 -0400

    feat: :tada: Started work on ID system
    Added pug template engine and other pacakages.

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.set('view engine', 'pug')
 
-app.get('/:org/:person', function (req, res, next) {
+app.get('/:id', (req, res) => {
+  const id = req.params.id
+})
+
+app.get('/:org/:person', function (req, res) {
     const org = req.params.org
     const person = req.params.person
     
     res.download(`./cards/${org}/${person}.vcf`, function (err) {
       if (err) {
-        res.redirect('/')
+        res.render('leg-error')
       }
     })
 })
 
-app.get('*', (req, res) => {
-  res.redirect('https://luhad.tech')
-})
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`vCard server listening on port ${port}`)
 })
