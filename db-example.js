e58aad827bc4cd20889322c7ed2c39d20f8d485e
Hayden <hayden@luhad.tech>
2023-04-27 09:36:00 -0400

    feat: :sparkles: Got the DB working
    Got neon db setup with proper tables. Has all the data required for Vcards. next commit will be handling that data to generate a Vcard

---
+++ db-example.js
@@ -0,0 +1,27 @@
+const { Pool, Client } = require('pg')
+require('dotenv').config();
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+const connectionString = `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`
+
+const pool = new Pool({
+  connectionString,
+})
+
+ async function query() {
+  const client = await pool.connect()
+ // inserting my info into the DB
+  try {
+    await client.query('BEGIN')
+    const queryText = 'INSERT INTO public.vcf_data (uuid, aliases, email, firstname, lastname, midlename, prefix, suffix, org, title, role, h_email, w_email, h_phone, c_phone, w_address, h_address, urls, bithday, gender) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20)'
+    await client.query(queryText, ['6babdb0a-6876-4d2d-afa2-ccb91e3a13b9', 'luhad/hayden', 'hderifield2005@outlook.com', 'Hayden', 'Derifield', null, null, null, 'Luhad Tech', 'Tech Director', null, 'hderifield2005@outlook.com', 'hayden@luhad.tech', '5025425419', '5025578013', null, null, null, null, 'male'])
+    await client.query('COMMIT')
+  } catch (e) {
+    await client.query('ROLLBACK')
+    throw e
+  } finally {
+    client.release()
+    return console.log('Data inserted.')
+  }
+ }
+ query();
